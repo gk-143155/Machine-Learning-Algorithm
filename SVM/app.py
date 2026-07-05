a80ee40ae8e685133e18f136cb81b91a666aaa4f
Gaurav <gaurav143155@gmail.com>
2026-07-04 17:43:49 -0700

    Docker suport

--- SVM/app.py
+++ SVM/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import joblib
 import pandas as pd
@@ -85,4 +86,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
